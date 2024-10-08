c63e689887b0509063c02b924efb13c94658d841
manikanta <155448771+manikantaoruganti@users.noreply.github.com>
2024-10-08 13:03:39 +0530

    Time: 10 ms (89.06%) | Memory: 58.1 MB (39.79%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-      //creating a hash set
-        HashSet<Integer>numbers = new HashSet<>();
-        //run a loop on 0 index to nums.length 
+        HashSet<Integer>result=new HashSet<>();
         for(int i=0;i<nums.length;i++){
-            if(numbers.contains(nums[i]))return true;
-            numbers.add(nums[i]);
-        
-            //in java we have contains function it iteratively moves and discard means does not allow duplicates it will run only for non duplicate occurance
+            if(result.contains(nums[i]))
+                return true;
+                
+                                         
+            result.add(nums[i]);
         }
-            return false;
+        return false;
+        
     }
 }
