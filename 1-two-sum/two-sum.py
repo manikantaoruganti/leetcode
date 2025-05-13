265e9f68efbba9c13c1f973cf97227440c07d18f
manikanta <155448771+manikantaoruganti@users.noreply.github.com>
2025-05-13 12:26:04 +0530

    Time: 0 ms (100.00%) | Memory: 18.8 MB (51.45%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,14 +1,11 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen = {}
         for i, num in enumerate(nums):
             complement = target - num
             if complement in seen:
                 return [seen[complement], i]
             seen[num] = i
-        return []  # The problem guarantees one valid solution
+        return [] 
