c3b479ee8592615bfe0bfe51e449c989667c86e1
manikanta <155448771+manikantaoruganti@users.noreply.github.com>
2025-05-14 11:17:28 +0530

    Time: 13 ms (49.46%) | Memory: 17.9 MB (11.57%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,5 +1,7 @@
-class Solution:
-    def countBits(self, n: int) -> List[int]:
+class Solution(object):
+    def countBits(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
         return [bin(i).count('1') for i in range(n + 1)]
-
-        
